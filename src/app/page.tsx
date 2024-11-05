3dc2b363481dc821a0ba92272458766de725dfdd
Ria <tprrmathew@gmail.com>
2024-11-05 14:43:29 -0800

    added smile.svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className="dark:invert" // when in dark mode it inverts colors
+          src="/smile.svg"
           alt="Next.js logo"
           width={180}
           height={38}
