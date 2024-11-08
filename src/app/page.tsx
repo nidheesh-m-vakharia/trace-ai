9524731ff31c5ed4cb1ddb82872560be1081d54c
Sabrina Piazza <sabrinapiazza@ucr-secure-31-10-13-172-151.wnet.ucr.edu>
2024-11-08 13:58:49 -0800

    Homepage Description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+
+    <div>
+      <main>
+        <h1>Trace AI</h1>
+        </main>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+          qui officia deserunt mollit anim id est laborum."</p>
+      </div>
   )
-  }
-  
+}
+
