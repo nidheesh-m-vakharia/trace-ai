7842f3ba3ddc8c944e30015496108f255215ed2f
Ria <tprrmathew@gmail.com>
2024-11-08 14:02:06 -0800

    formated image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Image
-          // className="dark:invert" when in dark mode it inverts colors
-          src="/smile.svg" // add a new file this will be added to public 
-          alt="smile logo" // 
+          src="/smile.svg" // add a new file this will be added to public
+          alt="smile logo" //
           width={200}
           height={100}
           priority
