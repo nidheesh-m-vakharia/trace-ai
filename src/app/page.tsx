a8e6a3ae0b966028f1b9c3ce3dbacbe1ee3f6ab5
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-11-20 17:54:49 -0800

    formatted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ import { redirect } from "next/navigation";
 import CreateButton from "@/components/createButton";
 
 export default function Home() {
-  throw new Error("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+  throw new Error(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  );
   return (
     <div>
       <h1 className="mt-10 dark:text-white">Trace AI</h1>
