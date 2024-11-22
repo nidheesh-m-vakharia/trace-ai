778f1ef214a84a6fd04638385fdf473a48ccda48
Ria <tprrmathew@gmail.com>
2024-11-22 13:59:33 -0800

    merge

--- src/components/createButton.tsx
+++ src/components/createButton.tsx
@@ -1,15 +1,10 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
 
-
 const CreateButton = () => {
-    return (
-        <Button onClick={()=>redirect("/create")}>
-            Create!
-        </Button>
-    )
-}
+  return <Button onClick={() => redirect("/create")}>Create!</Button>;
+};
 
 export default CreateButton;
