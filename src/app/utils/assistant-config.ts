56165d7f6c973b3a108259cb7196a77403140500
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-11-23 17:13:21 -0800

    changed workflow

--- src/app/utils/assistant-config.ts
+++ src/app/utils/assistant-config.ts
@@ -9,7 +9,7 @@ type ASSISTANT = {
 };
 
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY ?? "",
 });
 
 export const assistantId = process.env.OPENAI_ASSISTANT_ID ?? "";
